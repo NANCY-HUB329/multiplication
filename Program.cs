efca0bf175c5c382bed11af5742ac945169a87df
NANCY-HUB329 <mwanginancy333@gmail.com>
2023-12-06 09:04:32 +0300

    multiplication of 2 numbers

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace multiplication
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        { 
+
+//        Console.WriteLine("Enter first number");
+//             int a = Convert.ToInt32 (Console.ReadLine());
+//        Console.WriteLine("Enter second number");
+//            int b = Convert.ToInt32 (Console.ReadLine());
+
+//            int product = multiply(a , b);
+//            Console.WriteLine("The product is" + $" {product} ");
+
+//    }
+//        public static int multiply(int a, int b)
+//        {
+//            return a * b;
+//        }
+//    }
+//}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace multiplication
+//{
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+
+//        Console.WriteLine("Enter your name");
+//        string name = Console.ReadLine();
+//        Console.WriteLine("Hello"   +   name);
+
+
+//    }
+//}
+//    }
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+          int a = Convert.ToInt32  (Console.ReadLine());
+            Console.WriteLine("Enter the first number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result = divide(a, b);
+        }
+        public static divide(int a, int b);
+        Return(a%b);
+    }
+}
